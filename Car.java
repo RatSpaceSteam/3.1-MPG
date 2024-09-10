27d1a2a88d61a91f9c78fc25ff056ca8ae175505
RatSpaceSteam <145033829+RatSpaceSteam@users.noreply.github.com>
2024-09-10 15:10:14 +0000

    thx

--- Car.java
+++ Car.java
@@ -1,11 +1,29 @@
 public class Car {
-    private int odometerReading;
+    private int myStartMiles;
+    private int myEndMiles;
+    private double myGallonsUsed;
 
     public Car(int odometerReading) {
-        this.odometerReading = odometerReading;
+        myStartMiles = odometerReading;
+        myEndMiles = odometerReading;
+    }
+    
+    public int getOdometerReading() {
+        return myEndMiles;
     }
 
-    public int getOdometerReading() {
-        return odometerReading;
+    public void fillUp(int odometerReading, double gallons) {
+        myEndMiles = odometerReading;
+        myGallonsUsed += gallons;
+    }
+
+    public double calculateMPG() {
+        double milesPerGallon = (myEndMiles - myStartMiles) / myGallonsUsed;
+        return milesPerGallon;
+    }
+
+    public void resetMPG() {
+        myStartMiles = myEndMiles;
+        myGallonsUsed = 0;
     }
 }
