45d22eb408cfcd8445e202d86b4fb6bbb2291aa5
RatSpaceSteam <145033829+RatSpaceSteam@users.noreply.github.com>
2024-09-10 14:32:24 +0000

    getreading

---
+++ Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    private int odometerReading;
+
+    public Car(int odometerReading) {
+        this.odometerReading = odometerReading;
+    }
+
+    public int getOdometerReading() {
+        return odometerReading;
+    }
+}
